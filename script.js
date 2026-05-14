a31d7862738ef6dab8274df61cf9046c6fe7eb65
giabd-academy <sharifbhuyan178@gmail.com>
2026-05-14 17:10:59 +0600

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const API_URL = "https://script.google.com/macros/s/AKfycbyTOxQTg8sP-NwY2LgRnVR-1C7j6RcM1T6VXc1t8YW0hGDkQSxrAtJrtJgTI5UgfkJxtw/exec";
+const API_URL = "https://script.google.com/macros/s/AKfycbzokWW957tA1GAhZ5e8rBSFJRa1FSYehoWAg8T96siL/dev";
 
 function submitLead(){
 
