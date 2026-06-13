1b156711df599a1ac0119707abf4a4e1f5980020
giabd-academy <sharifbhuyan178@gmail.com>
2026-06-13 08:45:53 +0600

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-
-function googleTranslateElementInit() {
-  new google.translate.TranslateElement({
-    pageLanguage: 'en',
-    includedLanguages: 'en,bn,ja',
-    layout: google.translate.TranslateElement.InlineLayout.SIMPLE
-  }, 'google_translate_element');
-}
